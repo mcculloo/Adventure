522b1cfe544f862a206ba51fafd93e44a4f57445
jzara3115 <jzara3115@gmail.com>
2022-03-03 03:26:39 -0800

    dialogye betta

--- wheelin/Assets/Scripts/ahhellnawshutup.cs
+++ wheelin/Assets/Scripts/ahhellnawshutup.cs
@@ -9,11 +9,20 @@ public class ahhellnawshutup : MonoBehaviour
     public GameObject npc;
     public GameObject ui;
     public TextMeshProUGUI names;
+    public TextMeshProUGUI facts;
     public string realname;
     public string[] word;
 
 
     public whathesayin talkscript;
+
+    public float seelength;
+    RaycastHit hit;
+    private Transform _selection;
+
+    public bool resettalk = true;
+    bool start;
+    bool notlooking;
 
     void Start()
     {
@@ -22,16 +31,55 @@ public class ahhellnawshutup : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {    
+        NotLooking();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawLine(ray.origin, hit.point);
+        if(_selection != null){
+            notlooking = true;
+            resettalk = true;
+            _selection = null;
+        }
+        if(Physics.Raycast(ray, out hit, seelength)){
+            var selection = hit.transform;
+            if(selection.CompareTag("NPC")){
+                notlooking = false;
+                resettalk = false;
+                talkscript = selection.GetComponent<whathesayin>();
+                npc = selection.gameObject;
+                realname = npc.name;
+                names.text = realname;
+                word = talkscript.dialogue;
+                facts.text = word[talkscript.numinConv];
+                if(Input.GetKeyDown(KeyCode.E)){
+                    ui.SetActive(true);
+                    start = true;
+                }
+                if(Input.GetKeyDown(KeyCode.Mouse0)&& talkscript.numinConv != talkscript.maxnuminConv-1 && start == true){
+                    facts.text = word[talkscript.numinConv++];
+            } else if(Input.GetKeyDown(KeyCode.Mouse0)&& talkscript.numinConv == talkscript.maxnuminConv-1){
+                ui.SetActive(false);
+                resettalk = true;
+                start = false;
+            }
+            _selection = selection;
+            
+        }
+        } ResetTheTalk(); 
     }
+              
 
-    private void OnTriggerEnter(Collider col){
-        talkscript = col.GetComponent<whathesayin>();
-        npc = col.gameObject;
-        realname = npc.name;
-        names.text = realname;
-        word = talkscript.dialogue;
+        void ResetTheTalk(){
+            if(resettalk == true){
+                talkscript.numinConv = 0;
+                start = false;
+            }
+        }
+
+        void NotLooking(){
+            if (notlooking == true){
+                ui.SetActive(false);
+            }
+        }
+
     }
-
-}
--- wheelin/Assets/Scripts/whathesayin.cs
+++ wheelin/Assets/Scripts/whathesayin.cs
@@ -7,12 +7,16 @@ public class whathesayin : MonoBehaviour
 {
     public string[] dialogue;
     public string Name;
+    public int numinConv;
+    public int maxnuminConv;
 
     void Start()
     {
         if(Name == null) {
             Name = this.name;
         }
+        numinConv = 0;
+        maxnuminConv = dialogue.Length;
     }
 
     void Update()
